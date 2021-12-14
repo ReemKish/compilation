3a6bc4f909e90d227c9109fb919a481dc4472ded
yanivcogan <yanivcogan89@gmail.com>
2021-12-14 13:23:17 -0500

    ex3 setup + scope variable shadowing

---
+++ ex3/src/AST/AST_DEC_VAR.java
@@ -0,0 +1,98 @@
+package AST;
+
+import TYPES.*;
+import SYMBOL_TABLE.*;
+
+public class AST_DEC_VAR extends AST_DEC
+{
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public String type;
+	public String name;
+	public AST_EXP initialValue;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_DEC_VAR(String type,String name,AST_EXP initialValue)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		this.type = type;
+		this.name = name;
+		this.initialValue = initialValue;
+	}
+
+	/********************************************************/
+	/* The printing message for a declaration list AST node */
+	/********************************************************/
+	public void PrintMe()
+	{
+		/********************************/
+		/* AST NODE TYPE = AST DEC LIST */
+		/********************************/
+		if (initialValue != null) System.out.format("VAR-DEC(%s):%s := initialValue\n",name,type);
+		if (initialValue == null) System.out.format("VAR-DEC(%s):%s                \n",name,type);
+
+		/**************************************/
+		/* RECURSIVELY PRINT initialValue ... */
+		/**************************************/
+		if (initialValue != null) initialValue.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("VAR\nDEC(%s)\n:%s",name,type));
+
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (initialValue != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,initialValue.SerialNumber);		
+			
+	}
+
+	public TYPE SemantMe()
+	{
+		SYMBOL_TABLE_ENTRY t;
+	
+		/****************************/
+		/* [1] Check If Type exists */
+		/****************************/
+		t = SYMBOL_TABLE.getInstance().find(type);
+		if (t == null)
+		{
+			System.out.format(">> ERROR [%d:%d] non existing type %s\n",2,2,type);
+			System.exit(0);
+		}
+		
+		/**************************************/
+		/* [2] Check That Name does NOT exist */
+		/**************************************/
+		SYMBOL_TABLE_ENTRY prev = SYMBOL_TABLE.getInstance().find(name);
+		if (prev != null)
+		{
+			SYMBOL_TABLE_ENTRY scope = SYMBOL_TABLE.getInstance().getScope();
+			if(scope.prevtop_index < prev.prevtop_index){
+				/* if the name was already declared within the current scope*/
+				System.out.format(">> ERROR [%d:%d] variable %s already exists in scope\n",2,2,name);
+			}
+		}
+
+		/***************************************************/
+		/* [3] Enter the Function Type to the Symbol Table */
+		/***************************************************/
+		SYMBOL_TABLE.getInstance().enter(name,t.type);
+
+		/*********************************************************/
+		/* [4] Return value is irrelevant for class declarations */
+		/*********************************************************/
+		return null;		
+	}
+	
+}
