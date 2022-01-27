1fce567d8c10a84d65d841e39d637d464c5a3413
yanivcogan <yanivcogan89@gmail.com>
2022-01-27 17:51:14 -0500

    removed irrelevant AST classes from _skeleton folder

--- ex4/FOLDER_2_SRC/_skeleton/AST/SemanticException.java
+++
@@ -1,8 +0,0 @@
-package AST;
-public class SemanticException extends Exception { 
-		public int line;  /* the erronous line number in the source file */
-    public SemanticException(int line) {
-        super(String.format("Semantic error at line %d", line));
-        this.line = line;
-    }
-}
