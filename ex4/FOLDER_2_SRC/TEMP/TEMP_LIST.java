1e26946f89414d38b46b6e0e6821584fa015abc6
ReemKish <stonewow1@gmail.com>
2022-02-08 13:37:15 +0200

    Added toString to TEMP and TENP_LIST. for example: TEMP.toString() returns "t1" TEMP_LIST.toString() returns "t1, t2, t3"

--- ex4/FOLDER_2_SRC/TEMP/TEMP_LIST.java
+++ ex4/FOLDER_2_SRC/TEMP/TEMP_LIST.java
@@ -9,6 +9,9 @@ package TEMP;
 
 /*******************/
 /* PROJECT IMPORTS */
+
+import IR.IRcommandList;
+
 /*******************/
 
 public class TEMP_LIST
@@ -28,7 +31,32 @@ public class TEMP_LIST
         this.tail = tail;
     }
     public void append(TEMP t){
-        this.tail = this;
-        this.head = t;
+        if ((head == null) && (tail == null))
+        {
+            this.head = t;
+        }
+        else if ((head != null) && (tail == null))
+        {
+            this.tail = new TEMP_LIST(t,null);
+        }
+        else
+        {
+            TEMP_LIST it = tail;
+            while ((it != null) && (it.tail != null))
+            {
+                it = it.tail;
+            }
+            it.tail = new TEMP_LIST(t,null);
+        }
+    }
+
+    public String toString() {
+        String items = "";
+        TEMP_LIST curr = this;
+        while(curr != null && curr.head != null) {
+            items = items.concat(curr.head.toString() + ", ");
+            curr = curr.tail;
+        }
+        return items.substring(0, items.length()-2);
     }
 }
