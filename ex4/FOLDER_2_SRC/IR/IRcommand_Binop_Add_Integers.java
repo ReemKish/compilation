7c0d0a7e2199c79655ff051855d82275f8add576
yanivcogan <yanivcogan89@gmail.com>
2022-02-08 11:01:22 -0500

    truncate addition result within valid range for int

--- ex4/FOLDER_2_SRC/IR/IRcommand_Binop_Add_Integers.java
+++ ex4/FOLDER_2_SRC/IR/IRcommand_Binop_Add_Integers.java
@@ -30,6 +30,24 @@ public class IRcommand_Binop_Add_Integers extends IRcommand
 	/***************/
 	public void MIPSme()
 	{
+		/*TODO: add these overflow checks to all other binary ops*/
 		sir_MIPS_a_lot.getInstance().add(dst,t1,t2);
+		String over_max_label = "over_max_"+IR.getInstance().getLabelIndex();
+		String after_max_label = "after_max_fix_"+IR.getInstance().getLabelIndex();
+		String under_min_label = "under_min"+IR.getInstance().getLabelIndex();
+		String after_min_label = "after_min_fix_"+IR.getInstance().getLabelIndex();
+		// check if the result is over the maximum int value
+		sir_MIPS_a_lot.getInstance().ble(dst, IR.getInstance().maxIntTemp, after_max_label);
+		// if so, truncate it (label for code readability only)
+		sir_MIPS_a_lot.getInstance().label(over_max_label);
+		sir_MIPS_a_lot.getInstance().move(dst, IR.getInstance().maxIntTemp);
+		// check if the result is under the minimum int value
+		sir_MIPS_a_lot.getInstance().label(after_max_label);
+		sir_MIPS_a_lot.getInstance().ble(IR.getInstance().minIntTemp, dst, after_min_label);
+		// if so, truncate it (label for code readability only)
+		sir_MIPS_a_lot.getInstance().label(under_min_label);
+		sir_MIPS_a_lot.getInstance().move(dst, IR.getInstance().minIntTemp);
+		sir_MIPS_a_lot.getInstance().label(after_min_label);
+		// done!
 	}
 }
